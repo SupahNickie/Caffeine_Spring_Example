cdae412fc24693d9b1554f00e2e69eba6c8f0d99
SupahNickie <nicholascase@live.com>
2016-12-28 10:08:19 -0800

    fix update all images query

--- src/main/java/portfolio/queries/ImageQueries.java
+++ src/main/java/portfolio/queries/ImageQueries.java
@@ -4,7 +4,7 @@ public class ImageQueries {
 	public static String updateOtherImagesToNonHeroStatus() {
 		return 	"update images " + 
 				"set (is_hero_image) = (false) " +
-				"where id is not $1 " + 
-				"and project_id is $2";
+				"where id <> $1 " + 
+				"and project_id = $2";
 	}
 }
