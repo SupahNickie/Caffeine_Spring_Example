1bcfb0107a58d9b46825ba100af0c02ee6615acd
SupahNickie <nicholascase@live.com>
2016-12-28 16:10:19 -0800

    add authorization handling

---
+++ src/main/java/portfolio/services/AuthInterceptor.java
@@ -0,0 +1,24 @@
+package main.java.portfolio.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		if (isAuthorized(request)) {
+			return true;			
+		} else {
+			response.setStatus(503);
+			return false;
+		}
+	}
+	
+	boolean isAuthorized(HttpServletRequest request) {
+		return request.getMethod().equals("OPTIONS") || 
+				((request.getHeader("portfolio-authorization") != null) && 
+				(request.getHeader("portfolio-authorization").equals(System.getenv("PORTFOLIO_AUTHORIZATION_KEY"))));
+	}
+}
